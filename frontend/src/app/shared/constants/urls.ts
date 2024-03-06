0fda256463ef1a4d6bae6212815a78042ff3bc6e
Jonathan Costa <62775506+JonathanSantos2000@users.noreply.github.com>
2024-03-06 17:14:45 -0300

    fix: server error cors

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -1,6 +1,6 @@
-import { environment } from 'src/environments/environment';
+import { environment } from "src/environments/environment";
 
-const BASE_URL = environment.production ? 'http://localhost:5000' : '';
+const BASE_URL = environment.production? '' : 'http://localhost:5000';
 
 export const RECIPE_URL = BASE_URL + '/api/recipe';
 
